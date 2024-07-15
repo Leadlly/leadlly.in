16468abb925e3c18ae210b05e49d652d5a1b62af
Rohit Singh Rawat <rohitzrawat2003@gmail.com>
2024-07-16 03:52:01 +0530

    feat: Add buttons

--- src/components/Home/Mentor.tsx
+++ src/components/Home/Mentor.tsx
@@ -4,50 +4,58 @@ import Image from 'next/image';
 import IconWithLabel from '../shared/IconWIthLabel';
 import Reveal from '../shared/Reveal';
 import RevealImage from '../shared/RevealImage';
+import FloatButton from '../shared/FloatButton';
+import { useRef } from 'react';
+import { useInView } from 'framer-motion';
 
 type Props = {};
-const Mentor = (props: Props) => {
-	return (
-		<section>
-			<div className='text-primary-blue text-6xl font-bold mx-40 my-20'>
-				<Reveal>
-					<h1>Don't Go It Alone. </h1>
-				</Reveal>
-				<Reveal>
-					<h1>Succeed with your mentor.</h1>
-				</Reveal>
-			</div>
-			<RevealImage
-				rightItem={
-					<Image
-						alt='mentor'
-						height={439}
-						width={565}
-						src={'/assets/images/mentor.png'}
-					/>
-				}
-				leftItem={
-					<p className='text-[#5B437D] font-semibold text-xl w-96 text-left'>
-						Gain valuable insights and strategies from someone experienced in your field of study.
-					</p>
-				}
-				delay={0.2}
-				motionDivClass='flex items-center justify-evenly'
-				imagePosition='right' // Specify image position
-			/>
-			<div className='grid grid-cols-2 gap-y-14 p-28 py-14'>
-				{MentorPoints.map((item, index) => (
-					<IconWithLabel
-						key={index}
-						icon={<item.icon className='size-full' />}
-						label={item.label}
-						delay={(index + 1) * 0.15}
-					/>
-				))}
-			</div>
-			{/* <FloatButton text="Dive into planner"/> */}
-		</section>
-	);
+const Mentor = (props: Props) => {const ref = useRef(null);
+const isInView = useInView(ref, { amount: 0.4 });
+return (
+	<section ref={ref}>
+		<div className='text-primary-blue text-6xl font-bold mx-40 my-20'>
+			<Reveal>
+				<h1>Don't Go It Alone. </h1>
+			</Reveal>
+			<Reveal>
+				<h1>Succeed with your mentor.</h1>
+			</Reveal>
+		</div>
+		<RevealImage
+			rightItem={
+				<Image
+					alt='mentor'
+					height={439}
+					width={565}
+					src={'/assets/images/mentor.png'}
+				/>
+			}
+			leftItem={
+				<p className='text-[#5B437D] font-semibold text-xl w-96 text-left'>
+					Gain valuable insights and strategies from someone experienced in your field of study.
+				</p>
+			}
+			delay={0.2}
+			motionDivClass='flex items-center justify-evenly'
+			imagePosition='right' // Specify image position
+		/>
+		<div className='grid grid-cols-2 gap-y-14 p-28 py-14'>
+			{MentorPoints.map((item, index) => (
+				<IconWithLabel
+					key={index}
+					icon={<item.icon className='size-full' />}
+					label={item.label}
+					delay={(index + 1) * 0.15}
+				/>
+			))}
+		</div>
+
+		<FloatButton
+			isInView={isInView}
+			text='Know your mentor'
+		/>
+	</section>
+);
 };
 
 export default Mentor;
--- src/components/shared/FloatButton.tsx
+++ src/components/shared/FloatButton.tsx
@@ -1,38 +1,52 @@
-'use client'
-import React, { useState } from 'react';
-import { motion } from 'framer-motion';
-
+'use client';
+import React, { useEffect, useState } from 'react';
+import { AnimatePresence, motion, useAnimate } from 'framer-motion';
+import { Flag } from 'lucide-react';
 
 type Props = {
 	text: string;
 	additionalStyles?: string;
+	isInView: boolean;
 };
 
-const FloatButton: React.FC<Props> = ({ text, additionalStyles }) => {
-	const [visible, setVisible] = useState(true);
+const FloatButton: React.FC<Props> = ({ text, additionalStyles, isInView }) => {
+	const [scope, animate] = useAnimate();
+	const [flag, setFlag] = useState(false);
+	useEffect(() => {
+		const animateButton = async () => {
+			if (!flag) {
+				setFlag(true);
+				if (isInView) {
+					await animate('.buttonText', { opacity: 0, display: 'none' });
+					await animate('button', { scale: 1, y: 0 }, { duration: 0.2, delay: 0.1 });
+					await animate('button', { width: 280 }, { duration: 0.3, delay: 0.1 });
+					await animate('.buttonText', { display: 'block', opacity: 1, x: 0 }, { duration: 0.1 });
+				} else {
+					await animate('.buttonText', { opacity: 0, display: 'none' });
+					await animate('button', { width: 60 });
+					await animate('button', { scale: 0, y: 0 });
+				}
+				setFlag(false);
+			}
+		};
 
-	const handleAnimationComplete = () => {
-		setTimeout(() => {
-			setVisible(false);
-		}, 2000); // 2 seconds delay before vanishing
-	};
+		animateButton();
+	}, [isInView, animate]);
 
 	return (
-		<>
-			{visible && (
-				<motion.div
-					className={`fixed bottom-10 right-10 bg-blue-500 text-white p-4 rounded-full shadow-lg backdrop-blur-md bg-opacity-70 ${additionalStyles}`}
-					initial={{ opacity: 0, y: 50 }}
-					animate={{ opacity: 1, y: 0 }}
-					exit={{ opacity: 0, y: 50 }}
-					whileHover={{ scale: 1.1 }}
-					onAnimationComplete={handleAnimationComplete}
-					transition={{ duration: 0.5 }}
+		<AnimatePresence>
+			<div
+				className='sticky bottom-10 z-40 flex justify-center'
+				ref={scope}
+			>
+				<motion.button
+					className={`bg-blue-200/20 size-[60px] overflow-hidden backdrop-blur-lg text-slate-700 font-medium p-2 rounded-full shadow-lg ${additionalStyles}`}
+					style={{ scale: 0 }}
 				>
-					{text}
-				</motion.div>
-			)}
-		</>
+					<span className='opacity-0 buttonText text-xl -translate-x-10'>{text}</span>
+				</motion.button>
+			</div>
+		</AnimatePresence>
 	);
 };
 
